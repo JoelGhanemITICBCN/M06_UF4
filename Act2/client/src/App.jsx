2a7679750a54401cf399e497f43fba9c9aae4b54
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-04-04 18:15:18 +0200

    Fet

---
+++ Act2/client/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function Button({ text, onClick, isClick }) {
+  const className = esClick ? 'btnClick' : 'btnReiniciar';
+  return <button className={className} onClick={onClick}>{text}</button>;
+}
+
+function App() {
+  const [numClicks, setNumClicks] = useState(0);
+
+  const incrementNum = () => {
+    console.log('Incrementant el número');
+    setNumClicks(numClicks + 1);
+  };
+
+  const resetNum = () => {
+    console.log('Reiniciant el número');
+    setNumClicks(0);
+  };
+
+  return (
+    <div className="App">
+      <p>Comptador: {numClicks}</p>
+      <Button text="Clic" onClick={incrementNum} esClick={true} />
+      <Button text="Reiniciar" onClick={resetNum} esClick={false} />
+    </div>
+  );
+}
+
+export default App;
